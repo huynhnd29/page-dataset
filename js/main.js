ea83ef462b4bc95e980963c90018799f4b41ad82
huynh1234 <42998636+huynh1234@users.noreply.github.com>
2021-07-04 16:20:05 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+function myFunction(){
+    document.getElementById("listbox").classList.toggle("show");
+}
+
+
+function openFunction(){
+    document.getElementById("leftNav").classList.toggle("show");
+    document.querySelector(".main").classList.toggle("close");
+}
