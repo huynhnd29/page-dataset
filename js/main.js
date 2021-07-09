a1fb4f8842c46d4d8f3ea08267edf8048e23198c
huynh1234 <42998636+huynh1234@users.noreply.github.com>
2021-07-09 22:17:51 +0700

    reponsive

--- js/main.js
+++ js/main.js
@@ -8,3 +8,18 @@ function openFunction(){
     document.getElementById("leftNav").classList.toggle("show");
     document.querySelector(".main").classList.toggle("close");
 }
+
+
+
+
+var header = document.getElementById("sliderIcon");
+var btn = header.getElementsByClassName("ic");
+for (var i=0 ; i<btn.length ; i++){
+    btn[i].addEventListener("click",function(){
+        var current = document.getElementsByClassName("active");
+        if(current.length >0){
+            current[0].className = current[0].className.replace(" active","");
+        }
+        this.className += " active";
+    })
+}
