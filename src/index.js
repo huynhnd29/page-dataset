0ea831f643db69f3152e78840a20e893df4e7457
NgDMau <maunguyen.dinh@gmail.com>
2021-08-25 15:07:46 +0700

    add script tag to head in index.html

--- src/index.js
+++ src/index.js
@@ -7,10 +7,6 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Helmet>
-      <script type="text/javascript" id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/4a243de9d1f78a967090e5a3c/1a20a2afb031bc50f31890a05.js");</script>
-      <meta name="description" content="Nested component" />
-    </Helmet>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
